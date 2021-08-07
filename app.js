f1ecffc7dfd3c1209962aad1cfece032797b1d29
Sohail Khan <sohailkhan.sk@hotmail.com>
2021-08-08 01:06:38 +0500

    start server file :runner:

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const notificationRoutes = require('./routes/notification');
+
+app.use(bodyParser.json());
+cors();
+
+app.use('/notification', notificationRoutes);
 
 const PORT = process.env.NODE_SERVER_PORT || 8080;
 app.listen(PORT, () => {
