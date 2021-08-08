a772a0cf3bbfa6201d0ac3e0d314c09982303691
Sohail Khan <sohailkhan.sk@hotmail.com>
2021-08-08 14:23:08 +0500

    add controller changes

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,9 +1,4 @@
-const {
-    sendIndividualTextNotification,
-    sendIndividualPushNotification,
-    sendGroupTextNotification,
-    sendGroupPushNotification
-} = require('../queues/notification');
+const { sendGroupNotification, sendIndividualNotification } = require('../queues/notification');
 
 module.exports = {
     sendIndividualNotification: async (req, res, next) => {
@@ -12,14 +7,10 @@ module.exports = {
 
             const { type, ids, content } = req.body;
 
-            if(type == 'text'){
-                sendIndividualTextNotification(ids, content);
-            }else{
-                sendIndividualPushNotification(ids, content);
-            }
+            sendIndividualNotification(ids, content, type);
 
             res.json({
-                message: 'Notification sending process initiated!'
+                message: 'Notification sending process initiated'
             });
             
         } catch (error) {
@@ -31,14 +22,10 @@ module.exports = {
 
             const { type, ids, content } = req.body;
 
-            if(type == 'text'){
-                sendGroupTextNotification(ids, content)
-            }else{
-                sendGroupPushNotification(ids, content);
-            }
+            sendGroupNotification(ids, content, type);
 
             res.json({
-                message: 'Notification sending process initiated!'
+                message: 'Notification sending process initiated'
             });
             
         } catch (error) {
